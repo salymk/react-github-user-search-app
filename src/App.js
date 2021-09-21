2eca346e3742b23bc4c3bbaed6c9dc68c55d234a
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-21 17:09:01 -0600

    feat: add SearchBar inside main tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import './styles/App.scss';
 
 function App() {
   return (
     <>
       <Header />
+      <main className="container">
+        <SearchBar />
+      </main>
     </>
   );
 }
