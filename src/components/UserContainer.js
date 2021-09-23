7668be50cff130d3f6aa24c27ed64e7e2dc5eda2
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-22 21:26:26 -0600

    refactor: delete user-meta-info div container

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import UserHeader from './UserHeader';
+import UserLinks from './UserLinks';
 import UserStats from './UserStats';
 
 const UserContainer = () => (
   <div className="user-container">
     <UserHeader />
-    <div>
+    <div className="user-content">
       <p className="bio">
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
         Quisque volutpat mattis eros.
       </p>
 
-      <div className="user-meta-info">
-        <UserStats />
-        <UserStats />
-      </div>
+      <UserStats />
+      <UserLinks />
     </div>
   </div>
 );
