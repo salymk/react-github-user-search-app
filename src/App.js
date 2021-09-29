b64dbed966799e7e5a55152b26ca2b3395a6ad8c
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-29 11:32:49 -0600

    feat: use useProfile hook to get profile based on user input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import React, { useState } from 'react';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import './styles/App.scss';
 import UserContainer from './components/UserContainer';
+import useProfile from './hooks/useProfile';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [username, setUserName] = useState('salymk');
+  const { data, isLoading, isError, error, isFetching } = useProfile(username);
+
+  if (error) return `An error has occured: ${error.message}`;
+
+  console.log(data);
+  console.log(isError);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUserName(input);
+  };
+
   return (
     <>
       <Header />
       <main className="container">
-        <SearchBar />
-        <UserContainer />
+        <SearchBar
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onSubmit={handleSubmit}
+        />
+        {isLoading ? 'Loading...' : <UserContainer bio={data?.bio} />}
+        {/* <UserContainer /> */}
       </main>
     </>
   );
