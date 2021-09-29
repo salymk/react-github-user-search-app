bcd6085a4dda21f20f698fa8a3603cfb038b3fe9
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-29 11:33:09 -0600

    feat: create bio prop for bio

--- src/components/UserContainer.js
+++ src/components/UserContainer.js
@@ -1,16 +1,15 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import UserHeader from './UserHeader';
 import UserLinks from './UserLinks';
 import UserStats from './UserStats';
 
-const UserContainer = () => (
+const UserContainer = (props) => (
   <div className="user-container">
     <UserHeader />
     <div className="user-content">
-      <p className="bio">
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
-        Quisque volutpat mattis eros.
-      </p>
+      <p className="bio">{props.bio}</p>
 
       <UserStats />
       <UserLinks />
