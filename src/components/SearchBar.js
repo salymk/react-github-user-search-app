6d1311b4f1dc022d2e176b89afb44ca6da6006e6
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-29 11:31:26 -0600

    refactor: use form element as container for search input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,52 +1,60 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import SearchIcon from '../icon-components/SearchIcon';
 
-function getGithubProfile(username) {
-  return fetch(`https://api.github.com/users/${username}`).then((res) =>
-    res.json()
-  );
-}
+const SearchBar = (props) => (
+  // const [input, setInput] = useState('');
+  // const [username, setUserName] = useState('');
 
-const SearchBar = (props) => {
-  const [input, setInput] = useState('');
-  const [username, setUserName] = useState('salymk');
+  // const { data, error, isLoading, isError } = useQuery(
+  //   ['profile', username],
+  //   () =>
+  //     fetch(`https://api.github.com/users/${username}`).then((res) =>
+  //       res.json()
+  //     ),
+  //   {
+  //     enabled: Boolean(username),
+  //   }
+  // );
 
-  const { data, error, isLoading, isFetching } = useQuery('profile', () =>
-    fetch(`https://api.github.com/users/${username}`).then((res) => res.json())
-  );
+  // if (isLoading) return 'Loading...';
+  // if (error) return `An error has occured: ${error.message}`;
 
-  if (isLoading) return 'Loading...';
-  if (error) return `An error has occured: ${error.message}`;
+  // console.log(data);
+  // console.log(isError);
 
-  console.log(data);
-
-  return (
+  <>
     <div className="search-wrap">
-      <label className="sr-only" htmlFor="search">
-        Search for github user
-      </label>
-      <div className="search-wrap__search-box">
-        <input
-          id="search"
-          name="search"
-          placeholder="Search GitHub username..."
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          {...props}
-        />
-        <SearchIcon viewBox="0 0 24 24" className="search-icon" />
-        {/* <span className="error">{error || ''}</span> */}
-        <button
-          onClick={() => setUserName(input)}
-          className="search-btn"
-          type="submit"
-        >
-          Search
-        </button>
-      </div>
+      <form onSubmit={props.onSubmit}>
+        <label className="sr-only" htmlFor="search">
+          Search for github user <span />
+        </label>
+        <div className="search-wrap__search-box">
+          <input
+            id="search"
+            name="search"
+            placeholder="Search GitHub username..."
+            // value={input}
+            // onChange={(e) => setInput(e.target.value)}
+            {...props}
+          />
+          <SearchIcon viewBox="0 0 24 24" className="search-icon" />
+          <button
+            // onClick={() => {
+            //   setUserName(input);
+            //   setInput('');
+            // }}
+            // eslint-disable-next-line react/destructuring-assignment
+            className="search-btn"
+            type="submit"
+          >
+            Search
+          </button>
+        </div>
+      </form>
     </div>
-  );
-};
-
+  </>
+);
 export default SearchBar;
