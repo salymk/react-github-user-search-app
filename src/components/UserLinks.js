0448a82ad7010a0bb90450268a37af719427bb1c
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-30 15:31:28 -0600

    feat: create props and use prop-types for validation

--- src/components/UserLinks.js
+++ src/components/UserLinks.js
@@ -1,28 +1,31 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import CompanyIcon from '../icon-components/CompanyIcon';
 import LocationIcon from '../icon-components/LocationIcon';
 import TwitterIcon from '../icon-components/TwitterIcon';
 import WebsiteIcon from '../icon-components/WebsiteIcon';
 
-const UserLinks = () => (
+const UserLinks = (props) => (
   <>
     <div className="user-links">
       <ul>
         <li>
           <LocationIcon alt="Location" />
-          <p>San Francisco</p>
+          <p>{props.location}</p>
         </li>
         <li>
           <WebsiteIcon alt="Website link" />
-          <a href="https://github.blog">https://github.blog</a>
+          <a href="https://github.blog">{props.website}</a>
         </li>
         <li>
           <TwitterIcon alt="Twitter" />
-          <p>Not Available</p>
+          <p>{props.twitter ? props.twitter : 'Not Available'}</p>
         </li>
         <li>
           <CompanyIcon alt="Company" />
-          <p>@github</p>
+          <p>@{props.company}</p>
         </li>
       </ul>
     </div>
@@ -30,3 +33,10 @@ const UserLinks = () => (
 );
 
 export default UserLinks;
+
+UserLinks.propTypes = {
+  location: PropTypes.string.isRequired,
+  website: PropTypes.string.isRequired,
+  twitter: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+};
