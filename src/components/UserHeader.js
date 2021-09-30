51492cb4e8e37969cda576eb298ceb0103c07650
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-30 14:38:06 -0600

    feat: add props and prop validation with prop-types

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,28 +1,37 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import placeholderImg from '../assets/salym-profile.jpg';
+import PropTypes from 'prop-types';
 
-const UserHeader = () => (
+const UserHeader = (props) => (
   <>
     <div className="user-header">
       <div className="user-header__img">
-        <img src={placeholderImg} alt="placeholder" />
+        <img src={props.img} alt={`${props.name} profile`} />
       </div>
       <div className="user-header__info">
         <div className="name">
-          <h2>The Octocat</h2>
+          <h2>{props.name}</h2>
           <a
             className="username"
-            href="https://github.com/octocat"
+            href={props.url}
             target="_blank"
             rel="noreferrer"
           >
-            @octocat
+            @{props.login}
           </a>
         </div>
-        <p className="date">Joined 25 Jan 2011</p>
+        <p className="date">Joined {props.created_at}</p>
       </div>
     </div>
   </>
 );
 
 export default UserHeader;
+
+UserHeader.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  login: PropTypes.string.isRequired,
+  created_at: PropTypes.string.isRequired,
+};
