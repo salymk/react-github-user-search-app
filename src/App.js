52e21cd942759f1cb188a4e05d1a5be47d3ebbe6
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-10-01 16:00:05 -0600

    refactor: show the user-container when loading is successful

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import Header from './components/Header';
@@ -12,9 +13,7 @@ import UserStats from './components/UserStats';
 function App() {
   const [input, setInput] = useState('');
   const [username, setUserName] = useState('salymk');
-  const { data, isLoading, isError, error } = useProfile(username);
-
-  if (isError) return `An error has occured: ${error.message}`;
+  const { data, error, status } = useProfile(username);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,39 +29,39 @@ function App() {
           onChange={(e) => setInput(e.target.value)}
           onSubmit={handleSubmit}
         />
-        <UserContainer>
-          <div className="user-container">
-            {isLoading ? (
-              '...'
-            ) : (
-              <>
-                <UserHeader
-                  img={data.avatar_url}
-                  name={data.name}
-                  url={data.url}
-                  login={data.login}
-                  created_at={data.created_at}
-                />
-                <div className="user-content">
-                  <p className="bio">{data.bio}</p>
 
-                  <UserStats
-                    repos={data.public_repos}
-                    followers={data.followers}
-                    following={data.following}
-                  />
-                  <UserLinks
-                    location={data.location}
-                    website={data.blog}
-                    twitter={data.twitter_username}
-                    company={data.company}
-                  />
-                </div>
-              </>
-            )}
-          </div>
-        </UserContainer>
-        {/* <UserContainer /> */}
+        {status === 'loading' ? (
+          <p>Loading...</p>
+        ) : status === 'error?' ? (
+          <p>Error: {error.response.json()}</p>
+        ) : (
+          <UserContainer>
+            <div className="user-container">
+              <UserHeader
+                img={data?.avatar_url}
+                name={data?.name}
+                url={data?.url}
+                login={data?.login}
+                created_at={data?.created_at}
+              />
+              <div className="user-content">
+                <p className="bio">{data?.bio}</p>
+
+                <UserStats
+                  repos={data?.public_repos}
+                  followers={data?.followers}
+                  following={data?.following}
+                />
+                <UserLinks
+                  location={data?.location}
+                  website={data?.blog}
+                  twitter={data?.twitter_username}
+                  company={data?.company}
+                />
+              </div>
+            </div>
+          </UserContainer>
+        )}
       </main>
     </>
   );
