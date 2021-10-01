ae13dbd85b75dc136dafd53bd9a1833c5a378db8
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-10-01 15:59:34 -0600

    refactor: handle errors
    - got to do some research, my errors are not showing up in the ui

--- src/hooks/useProfile.js
+++ src/hooks/useProfile.js
@@ -3,10 +3,13 @@ import { useQuery } from 'react-query';
 export default function useProfile(username) {
   return useQuery(
     ['profile', username],
-    () =>
-      fetch(`https://api.github.com/users/${username}`).then((res) =>
-        res.json()
-      ),
+    async () => {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    },
     {
       enabled: Boolean(username),
     }
