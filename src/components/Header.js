46712dd1cd64228346c130443911052886c55b19
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-10-05 14:35:56 -0600

    feat: configure toggle button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ThemeContext from '../contexts/theme';
 import MoonIcon from '../icon-components/MoonIcon';
+import SunIcon from '../icon-components/SunIcon';
 
-const Header = () => (
-  <header className="container header">
-    <nav>
-      <h1>devfinder</h1>
-      <button className="toggle-btn" type="button">
-        <p>Dark</p>
-        <MoonIcon fill="red" />
-      </button>
-    </nav>
-  </header>
-);
+const Header = ({ toggleTheme }) => {
+  const theme = React.useContext(ThemeContext);
+  return (
+    <header className="container header">
+      <nav>
+        <h1>devfinder</h1>
+        <button onClick={toggleTheme} className="toggle-btn" type="button">
+          {theme === 'light' ? (
+            <div className="dark-icon">
+              <p>Dark</p>
+              <MoonIcon />
+            </div>
+          ) : (
+            <div className="light-icon">
+              <p>Light</p>
+              <SunIcon />
+            </div>
+          )}
+        </button>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
+
+Header.propTypes = {
+  toggleTheme: PropTypes.func.isRequired,
+};
